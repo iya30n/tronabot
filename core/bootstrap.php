1a06c7a631549a5e4dd158e3f95be0cd2c10558e
yasin <yascanary0@gmail.com>
2021-10-15 19:26:10 +0330

    comment binding database class

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,7 +3,7 @@ use App\Core\App;
 
 App::bind('config', require('config.php'));
 
-App::bind('database', new QueryBuilder(
-    Connection::make(App::get('config')['database'])
-));
+// App::bind('database', new QueryBuilder(
+//     Connection::make(App::get('config')['database'])
+// ));
 
